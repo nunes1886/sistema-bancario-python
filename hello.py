6f6465117e5e57d9a6ee9c97fc54a80652cc2039
Gledson Nunes <nuness.gledson@gmail.com>
2023-07-17 14:30:54 -0300

    Otimizando sistema bancário com funções python

---
+++ hello.py
@@ -0,0 +1,15 @@
+#print("Cadastro de veículo".center(32, "*"))
+
+#nome = input("Nome do proprietário: ")
+#marca = input("Marca: ")
+#modelo = input("Modelo: ")
+#cor = input("Cor: ")
+#chassi = input("Chassi: ")
+
+print("Conversor de Centímetro para Metro")
+
+centi = float(input("Centímetro: "))
+
+mult = centi * 100
+
+print(f"Valor em metro: ", mult)
